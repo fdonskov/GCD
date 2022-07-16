ea5c7659362167fdf0aa57e60257dfa65ec332d5
Fedor Donskov <fedya_donskov@mail.ru>
2022-07-16 22:30:32 +0300

    Add url image and ActivityIndicator.

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        
+        // Выполняется когда я хочу получить значение image
+        get {
+            return imageView.image
+        }
+        
+        // Выполняется при установке нового значения для свойства
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
         
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
     }
 }
