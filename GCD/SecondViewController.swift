4e6523edd91861616528e399e8046cfcbcf44e55
Fedor Donskov <fedya_donskov@mail.ru>
2022-07-16 22:08:46 +0300

    Added a button, an image, and a ViewController.

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Федор Донсков on 16.07.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
